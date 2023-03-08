c3248b95ad30aab0fc42a9774130ad84025d1184
Khaatora <ahosari20@gmail.com>
2023-03-08 05:41:28 +0200

    added firebase error codes

--- lib/shared/components/firebase_error_codes.dart
+++ lib/shared/components/firebase_error_codes.dart
@@ -1,4 +1,8 @@
-class FirebaseErrorCodes{
+class FirebaseErrorCodes {
   static const String weakPassword = "weak-password";
-  static const String emailInUse = "email-already-in-use'";
+  static const String emailInUse = "email-already-in-use";
+  static const String userDisabled = "user-disabled*";
+  static const String userNotFound = "user-not-found*";
+  static const String wrongPassword = "wrong-password";
+
 }
