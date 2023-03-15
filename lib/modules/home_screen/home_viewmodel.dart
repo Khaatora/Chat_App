a3dceb8e169cdf5312d6fac7cef3f3dddca13c8a
Khaatora <ahosari20@gmail.com>
2023-03-15 10:20:18 +0200

    finished homescreen

--- lib/modules/home_screen/home_viewmodel.dart
+++ lib/modules/home_screen/home_viewmodel.dart
@@ -1,7 +1,19 @@
 import 'package:chat_own/base_class.dart';
+import 'package:chat_own/models/chat_room.dart';
+import 'package:chat_own/shared/network/cloud_firestore_utils.dart';
 
 import 'home_navigator.dart';
 
-class HomeViewModel extends BaseViewModel<HomeNavigator>{
+class HomeViewModel extends BaseViewModel<HomeNavigator> {
+  List<ChatRoomModel> rooms = [];
 
+  void readRooms() {
+    CloudFirestoreUtils.readRoomsFromFirestore()
+        .then((rooms) => this.rooms = rooms)
+        .catchError(
+      (error) {
+        navigator.showMessage(error.toString());
+      },
+    );
+  }
 }
