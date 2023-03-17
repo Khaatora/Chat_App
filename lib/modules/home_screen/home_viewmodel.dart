275e6ba9574ab1c0c1d34dac11fdc96e2cdeb819
Khaatora <ahosari20@gmail.com>
2023-03-17 07:30:38 +0200

    added steam rooms function

--- lib/modules/home_screen/home_viewmodel.dart
+++ lib/modules/home_screen/home_viewmodel.dart
@@ -1,19 +1,19 @@
 import 'package:chat_own/base_class.dart';
 import 'package:chat_own/models/chat_room.dart';
 import 'package:chat_own/shared/network/cloud_firestore_utils.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'home_navigator.dart';
 
 class HomeViewModel extends BaseViewModel<HomeNavigator> {
-  List<ChatRoomModel> rooms = [];
+  List<ChatRoomModel> chatlist = [];
 
   void readRooms() {
-    CloudFirestoreUtils.readRoomsFromFirestore()
-        .then((rooms) => this.rooms = rooms)
-        .catchError(
-      (error) {
-        navigator.showMessage(error.toString());
-      },
-    );
+    CloudFirestoreUtils.readRoomsFromFirestore().asStream();
+
   }
+
+  Stream<QuerySnapshot<ChatRoomModel>> readRoomsAsStream(){
+   return CloudFirestoreUtils.readRoomsFromFirestoreAsStream();
+}
 }
